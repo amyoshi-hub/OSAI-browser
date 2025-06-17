b36330f44aeb358342c525d99c47510a53154238
amyoshi9 <narinarit59@gmail.com>
2025-06-17 23:56:55 +0900

    welcome to type script

--- src_view/backup/index.js
+++
@@ -1,7 +0,0 @@
-import { CtrlElement } from "./ctrl-element.js"
-import { ResultElement } from "./result-element.js"
-import { AppElement } from "./app-element.js"
-
-customElements.define("control-element", CtrlElement)
-customElements.define("result-element", ResultElement)
-customElements.define("app-element", AppElement)
--- src_view/backup/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-    return <h1>Hello, Tauri with React!</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
-

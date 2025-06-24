26b794d9a7a8082bba3f53e980ea2ddf0e62d621
amyoshi9 <narinarit59@gmail.com>
2025-06-25 07:11:32 +0900

    websocket server added

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,13 @@ pub mod server;
 pub mod server_signal;
 pub mod client;
 pub mod file_copy;
+pub mod websocket;
 
 use server::start_server;
 use client::send_text;
 use file_copy::{process_and_add_world};
 use file_copy::{get_world_list, open_world};
+use websocket::start_websocket_server;
 
 
 #[tokio::main]
@@ -24,6 +26,7 @@ async fn main() {
             get_world_list,
 //            read_text_file,
             open_world,
+            start_websocket_server,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
